2f32d7abff7a4d22fe9a8f4e71364262f7153dc5
Modev101 <bettachemohamed333@gmail.com>
2026-08-05 14:55:00 +0100

    add mail module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    MailModule,
   ],
 })
 export class AppModule {}
