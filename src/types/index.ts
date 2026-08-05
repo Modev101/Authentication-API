5f2037b3e4038826c097a0c0a7bd2b654dca7592
Modev101 <bettachemohamed333@gmail.com>
2026-08-05 14:11:03 +0100

    add auth types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,23 @@
+import { Roles } from '@prisma/client';
+import { Request } from 'express';
+
 export type JwtPayload = {
   sub: string;
   email: string;
+  role: Roles;
 };
+
+export type JwtUser = {
+  userId: string;
+  email: string;
+};
+
+export type AuthUser = {
+  userId: string;
+  email: string;
+  role: Roles;
+};
+
+export interface AuthRequest extends Request {
+  user: AuthUser;
+}
