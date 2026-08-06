5f3eb562ae314dca079e1a3d61477d3ce8d2787a
Modev101 <bettachemohamed333@gmail.com>
2026-08-06 20:48:51 +0100

    import audit log in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MailModule } from './mail/mail.module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { AuditModule } from './audit/audit.module';
 
 @Module({
   imports: [
@@ -22,6 +23,7 @@ import { APP_GUARD } from '@nestjs/core';
       isGlobal: true,
     }),
     MailModule,
+    AuditModule,
   ],
   providers: [
     {
